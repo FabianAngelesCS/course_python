b230be6ee57ca297db3981988d7e7a7c6c7fbd24
Fabian <cruzfabian292@mail.com>
2024-03-04 23:24:42 -0600

    leccion: como se ejecuta el codigo python

---
+++ how_python_code_execute.py
@@ -0,0 +1,31 @@
+"""
+    
+    Python es un lenguaje de programación interpretado, lo que significa que el código fuente Python es
+    ejecutado directamente por un intérprete de Python sin necesidad de ser compilado previamente a un
+    código de máquina. Aquí está el proceso básico de ejecución de un código Python:
+
+    Interpretación del código fuente: 
+    Cuando ejecutas un programa Python, el intérprete de Python lee el código fuente línea por línea y lo 
+    convierte en instrucciones ejecutables.
+
+    Análisis léxico y sintáctico: 
+    El intérprete de Python primero realiza un análisis léxico y sintáctico del código para asegurarse de que
+    esté escrito correctamente según las reglas del lenguaje Python. Si encuentra algún error de sintaxis, 
+    genera un mensaje de error.
+
+    Generación de bytecode: 
+    Después de que el código fuente ha sido analizado y validado, el intérprete de Python compila el código 
+    en una forma intermedia llamada "bytecode". El bytecode es un código de nivel de máquina específico de
+    Python, no del sistema en el que se ejecuta.
+
+    Ejecución del bytecode: 
+    Una vez generado el bytecode, el intérprete de Python ejecuta este bytecode en una máquina virtual Python
+    (llamada PVM - Python Virtual Machine). La PVM es responsable de ejecutar el bytecode en el sistema 
+    operativo subyacente.
+
+    Resultados de la ejecución:
+    Durante la ejecución, el intérprete de Python puede generar resultados, como la salida de la función 
+    print(), el valor devuelto por una función, mensajes de error, etc.
+    
+    
+"""
---
+++ what_is_program.py
@@ -0,0 +1,19 @@
+"""
+---Qué es un programa?---
+
+  En el contexto de Python, un programa es un conjunto de instrucciones escritas en el lenguaje de 
+  programación Python que se ejecutan para realizar una tarea específica. Un programa Python puede ser
+  tan simple como imprimir un mensaje en la pantalla o tan complejo como una aplicación web o un sistema
+  de inteligencia artificial.
+
+  EN PYTHON:
+  Un programa Python típicamente consiste en una secuencia de declaraciones y comandos que se ejecutan
+  secuencialmente. Estas declaraciones pueden incluir asignaciones de variables, estructuras de control
+  como condicionales y bucles, llamadas a funciones y métodos, entre otras cosas.
+    
+Por ejemplo, aquí hay un programa Python muy simple que imprime "¡Hola, mundo!":
+
+python
+Copy code
+print("¡Hola, mundo!")  
+"""
